87dd2f70417b19918262b82a4dc4258eca350acd
Ashddin Huq <ashddin.huq@northsouth.edu>
2020-11-18 10:45:14 +0600

    Homework

---
+++ Homework Submission/Homework 1/Question_1.asm
@@ -0,0 +1,17 @@
+
+org 100h
+
+
+    MOV AX, 1
+    
+    ADD AX, 2
+    
+    ADD AX, 3
+    
+    MOV CX, AX
+
+ret
+
+
+
+
---
+++ Homework Submission/Homework 1/Question_2.asm
@@ -0,0 +1,16 @@
+
+org 100h
+
+    MOV AX, 2
+    ADD AX, 4
+    ADD AX, 6
+    ADD AX, 8
+    ADD AX, 10
+    
+    MOV CX,AX
+
+ret
+
+
+
+
