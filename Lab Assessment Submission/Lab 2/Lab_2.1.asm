31210e8378f4b23dc32955e215cbc4284dca3dbe
Ashddin Huq <ashddin.huq@northsouth.edu>
2020-11-18 09:52:00 +0600

    Lab_2

---
+++ Lab Assessment Submission/Lab 2/Lab_2.1.asm
@@ -0,0 +1,12 @@
+
+org 100h
+
+; add your code here  
+
+a DB 5 DUP (3)
+
+ret
+
+
+
+
---
+++ Lab Assessment Submission/Lab 2/Lab_2.asm
@@ -0,0 +1,19 @@
+
+org 100h
+
+; add your code here
+    K1 EQU 1
+    K2 EQU 2
+    K3 EQU 3
+    
+    ADD AX,K1
+    ADD AX,K2
+    ADD AX,K3 
+    
+    MOV BX, AX
+
+ret
+
+
+
+
